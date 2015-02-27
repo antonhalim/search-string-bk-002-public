3692c28c61ca4ee109dce79ddeb88b79f6ed6131
Katie Hoffman <ktahoffman@gmail.com>
2015-02-27 10:35:59 -0500

    change boolean to false

--- spec/search_string_spec.rb
+++ spec/search_string_spec.rb
@@ -1,5 +1,5 @@
 describe '#contains_needle?' do
-  it 'returns true if string present' do 
+  it 'returns true if string present' do
     expect(contains_needle?("iron", "Flatiron School")).to eq(true)
     expect(contains_needle?("log", "zoology")).to eq(true)
   end
@@ -9,8 +9,8 @@ describe '#contains_needle?' do
     expect(contains_needle?("ZOO", "zOology")).to eq(true)
   end
 
-  it 'returns false if string is not present' do 
+  it 'returns false if string is not present' do
     expect(contains_needle?("pickle", "Flatiron School")).to eq(false)
-    expect(contains_needle?("cat", "zoology")).to eq(true)
+    expect(contains_needle?("cat", "zoology")).to eq(false)
   end
 end
