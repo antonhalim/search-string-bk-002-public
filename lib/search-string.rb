b3c07d92daea3d50bde639a025de59d7e6e30c0e
axton21 <axton_n@yahoo.com>
2015-03-17 09:12:00 -0400

    yay

--- lib/search-string.rb
+++ lib/search-string.rb
@@ -1,3 +1,7 @@
-def contains_string?(needle, haystack)
-  
-end
+require 'pry'
+
+  def contains_needle?(needle, haystack)
+    @needle, @haystack = needle.downcase, haystack.downcase
+        result = @haystack.scan(@needle)
+        result.empty? ? false : true
+  end
